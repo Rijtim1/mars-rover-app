8e9f95670379d3220d6b2cc3339b1e3345b9226a
Rijan Timsina <regiontimsina@gmail.com>
2024-07-03 09:57:29 -0400

    Update ImageGrid.js

--- src/app/components/ImageGrid.js
+++ src/app/components/ImageGrid.js
@@ -3,7 +3,7 @@ import RoverImage from './RoverImage';
 
 const ImageGrid = ({ photos }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
       {photos.map(photo => (
         <RoverImage key={photo.id} imgSrc={photo.img_src} camera={photo.camera.full_name} date={photo.earth_date} />
       ))}
