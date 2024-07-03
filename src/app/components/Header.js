2cc913749d4214ea1392ff9306f34e30b4f187e4
Rijan Timsina <regiontimsina@gmail.com>
2024-07-03 09:52:56 -0400

    Update Header.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,29 +4,29 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <nav>
-        <ul className="flex space-x-4">
-          <li>
-            <Link href="/" className="flex items-center space-x-2 hover:text-gray-300">
+    <header className="bg-gray-800 text-white p-4 flex items-center">
+      <nav className="w-full">
+        <ul className="flex justify-between w-full">
+          <li className="flex-1">
+            <Link href="/" className="flex items-center space-x-2 hover:text-gray-300 justify-center">
               <Image src="/logo.png" alt="Mars Rover Photos" width={30} height={30} />
               <span>Home</span>
             </Link>
           </li>
-          <li>
-            <Link href="/curiosity" className="flex items-center space-x-2 hover:text-gray-300">
+          <li className="flex-1">
+            <Link href="/curiosity" className="flex items-center space-x-2 hover:text-gray-300 justify-center">
               <Image src="/curiosity-logo.png" alt="Curiosity" width={30} height={30} />
               <span>Curiosity</span>
             </Link>
           </li>
-          <li>
-            <Link href="/opportunity" className="flex items-center space-x-2 hover:text-gray-300">
+          <li className="flex-1">
+            <Link href="/opportunity" className="flex items-center space-x-2 hover:text-gray-300 justify-center">
               <Image src="/opportunity-logo.png" alt="Opportunity" width={30} height={30} />
               <span>Opportunity</span>
             </Link>
           </li>
-          <li>
-            <Link href="/spirit" className="flex items-center space-x-2 hover:text-gray-300">
+          <li className="flex-1">
+            <Link href="/spirit" className="flex items-center space-x-2 hover:text-gray-300 justify-center">
               <Image src="/spirit-logo.png" alt="Spirit" width={30} height={30} />
               <span>Spirit</span>
             </Link>
