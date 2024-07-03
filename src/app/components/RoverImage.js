9600bc34bf25b34c60d062d199bd819714d3e3a9
Rijan Timsina <regiontimsina@gmail.com>
2024-07-03 09:57:33 -0400

    Update RoverImage.js

--- src/app/components/RoverImage.js
+++ src/app/components/RoverImage.js
@@ -3,9 +3,10 @@ import Image from 'next/image';
 
 const RoverImage = ({ imgSrc, camera, date }) => {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md">
-      <Image src={imgSrc} alt={`${camera} - ${date}`} width={400} height={400} layout="responsive" className="rounded-md" />
-      <p className="mt-2 text-gray-700">{camera} - {date}</p>
+    <div className="p-4 bg-white rounded-lg shadow-md flex flex-col items-center">
+      <Image src={imgSrc} alt={`${camera} - ${date}`} width={400} height={400} className="rounded-md mb-4" />
+      <p className="text-center text-gray-700">{camera}</p>
+      <p className="text-center text-gray-500">{date}</p>
     </div>
   );
 };
