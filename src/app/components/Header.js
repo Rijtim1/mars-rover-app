afb71f45125352bfdbbe051a26c7d5a52885df07
Rijan Timsina <regiontimsina@gmail.com>
2024-07-17 10:33:43 -0400

    feat: removed opportunity and spirit
    removed spirit and opportunity as they no longer display the images correctly
    the are "retired"

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
               <span style={{ fontSize: '14px' }}>Curiosity</span>
             </Link>
           </li>
-          <li className="flex-1">
+          {/* <li className="flex-1">
             <Link href="/opportunity" className="flex flex-col items-center space-y-1 hover:text-gray-300 justify-center">
               <Image src="/opportunity-logo.png" alt="Opportunity" width={44} height={44} />
               <span style={{ fontSize: '14px' }}>Opportunity</span>
@@ -30,7 +30,7 @@ const Header = () => {
               <Image src="/spirit-logo.png" alt="Spirit" width={44} height={44} />
               <span style={{ fontSize: '14px' }}>Spirit</span>
             </Link>
-          </li>
+          </li> */}
         </ul>
       </nav>
     </header>
