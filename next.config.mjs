14525b8ad14234fda80d44da946e141eee82278b
Rijan Timsina <regiontimsina@gmail.com>
2024-07-17 10:20:03 -0400

    fix: allow both http and https protocols for remote images
    - Updated next.config.js to include both http and https protocols for remote image patterns
    - Added remotePatterns for mars.jpl.nasa.gov, mars.nasa.gov, and apod.nasa.gov with both http and https
    - Included a wildcard pattern to allow any hostname with both http and https protocols

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,42 @@ const nextConfig = {
         {
           protocol: 'https',
           hostname: 'mars.jpl.nasa.gov',
+          pathname: '/**',
+        },
+        {
+          protocol: 'http',
+          hostname: 'mars.jpl.nasa.gov',
+          pathname: '/**',
         },
         {
           protocol: 'https',
           hostname: 'mars.nasa.gov',
+          pathname: '/**',
+        },
+        {
+          protocol: 'http',
+          hostname: 'mars.nasa.gov',
+          pathname: '/**',
         },
         {
           protocol: 'https',
           hostname: 'apod.nasa.gov',
+          pathname: '/**',
+        },
+        {
+          protocol: 'http',
+          hostname: 'apod.nasa.gov',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: '**',
+          pathname: '/**',
+        },
+        {
+          protocol: 'http',
+          hostname: '**',
+          pathname: '/**',
         },
       ],
     },
