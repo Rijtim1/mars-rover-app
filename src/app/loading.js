7e3592af94abdef35988a6c62d1a360b4b53a552
Rijan Timsina <regiontimsina@gmail.com>
2024-07-17 10:20:37 -0400

    feat: implement global loading indicator using loading.js
    - Added loading.js file to provide a global loading indicator with a spinner
    - Created Loading component for reusable loading UI
    - Updated globals.css with styles for the loading spinner
    - Configured next.config.js to allow remote images from various sources
    - Ensured loading indicator is displayed during route changes and data fetching

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+// /pages/loading.js
+export default function Loading() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="loader">
+            <h1>Loading...</h1>
+        </div>
+      </div>
+    );
+  }
+  
